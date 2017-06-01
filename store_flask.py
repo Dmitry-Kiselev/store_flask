48e00bf8483569d10ddf684fa5b2cb0d2b045b35
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-01 17:22:15 +0300

    configure app

--- store_flask.py
+++ store_flask.py
@@ -1,6 +1,35 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+from extensions import login_manager
+from users.models import User, AnonymousUser
+from users.views import users
+
+
+def configure_blueprints(app):
+    app.register_blueprint(users, url_prefix='/auth')
+
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    configure_blueprints(app)
+
+    return app
+
+
+app = create_app()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_instance = User.query.filter_by(id=user_id).first()
+    if user_instance:
+        return user_instance
+    else:
+        return None
+
+login_manager.anonymous_user = AnonymousUser
 
 
 @app.route('/')
