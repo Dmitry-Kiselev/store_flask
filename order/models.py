5ccd35dad7af08fbeff69e4fb76ff337da62a167
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-05 15:02:12 +0300

    add order app

---
+++ order/models.py
@@ -0,0 +1,79 @@
+import datetime
+
+from decimal import Decimal
+from sqlalchemy_utils.types.choice import ChoiceType
+
+from basket.models import Basket
+from database import db
+
+
+class Order(db.Model):
+    class ORDER_STATUS:
+        PENDING = 0
+        FAILED = 1
+        PROCESSING = 2
+        COMPLETED = 3
+
+        STATUS_CHOICES = (
+            (PENDING, 'Pending payment'),
+            (FAILED, 'Failed'),
+            (PROCESSING, 'Processing'),
+            (COMPLETED, 'Completed')
+        )
+
+    __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key=True)
+    basket_id = db.Column(db.Integer, db.ForeignKey('baskets.id'))
+    status = db.Column(db.SmallInteger, ChoiceType(ORDER_STATUS.STATUS_CHOICES),
+                       default=ORDER_STATUS.PENDING)
+    discount_id = db.Column(db.Integer, db.ForeignKey('discounts.id'), nullable=True)
+
+    def __init__(self, basket_id, discount_id):
+        self.basket_id = basket_id
+        self.discount_id = discount_id
+
+
+    @property
+    def total_price(self):
+        basket = Basket.query.get(self.basket_id)
+        shipping_price = basket.shipping_price
+        if not self.discount_id:
+            return basket.total_price + Decimal(shipping_price)
+        discount = Discount.query.get(self.discount_id)
+
+        if discount.in_percent:
+            return basket.total_price * (
+                (100 - discount.value) / 100) + shipping_price
+        else:
+            return basket.total_price - discount.value + Decimal(shipping_price)
+
+    @property
+    def get_discount_val(self):
+        if not self.discount:
+            return 0
+        if self.discount.in_percent:
+            return self.basket.total_price - (
+                self.basket.total_price * Decimal((100 - self.discount.value) / 100))
+        else:
+            return self.basket.total_price - (
+                self.basket.total_price - self.discount.value)
+
+    def get_status(self):
+        return dict(Order.ORDER_STATUS.STATUS_CHOICES)[self.status]
+
+
+class Discount(db.Model):
+    __tablename__ = "discounts"
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    value = db.Column(db.DECIMAL)
+    in_percent = db.Column(db.Boolean, default=False)
+    available_from = db.Column(db.DateTime)
+    available_until = db.Column(db.DateTime)
+
+    def is_active(self):
+        now = datetime.datetime.now()
+        return self.available_from >= now and self.available_until <= now
+
+    def mark_as_used(self):
+        self.is_used = True
---
+++ order/views.py
@@ -0,0 +1,54 @@
+from flask import render_template, flash, request, redirect, Blueprint
+from flask.views import MethodView
+from flask_login import current_user
+from stripe import InvalidRequestError
+
+from database import db
+from order.models import Order
+from payment.forms import PaymentForm
+from payment.models import Payment
+from payment.providers import PaymentProviders
+
+orders = Blueprint("orders", __name__)
+
+
+class OrderCreateView(MethodView):
+    payment_provider_class = PaymentProviders.get_default_provider()
+    form_class = PaymentForm
+    template_name = 'order/checkout.html'
+
+    def get(self):
+        return render_template(self.template_name, form=self.form_class())
+
+    def post(self):
+        form = self.form_class(request.form)
+        if form.validate():
+            basket = current_user.get_basket
+            discount_id = current_user.discount.id if current_user.discount else None
+            order = Order(basket_id=basket.id, discount_id=discount_id)
+            basket.submit()
+
+            number = form.number.data
+            exp_month = form.expiration_month.data
+            exp_year = form.expiration_year.data
+            cvc = form.cvc.data
+            provider = self.payment_provider_class()
+
+            try:
+                charge_id = provider.charge(number, exp_month, exp_year, cvc,
+                                            order.total_price)
+            except InvalidRequestError:
+                charge_id = None
+                flash(
+                    'Some error happened during checkout process. Please, try again later')
+
+            payment = Payment(charge_id=charge_id, order_id=order.id)
+            db.session.add(payment)
+            db.session.commit()
+            flash(
+                'Thanks for your order!')
+            return redirect('/')
+
+
+orders.add_url_rule("/checkout/",
+                    view_func=OrderCreateView.as_view('checkout'))
