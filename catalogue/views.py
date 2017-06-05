1ba1b1fac3b34b8a0c1d758c123cd4ec5cba9fea
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-05 09:12:16 +0300

    use add_url_rule for catalogue urls instead of decorators

--- catalogue/views.py
+++ catalogue/views.py
@@ -5,20 +5,23 @@ from .models import Category, Product
 catalogue = Blueprint("catalogue", __name__)
 
 
-@catalogue.route("/")
 def index():
     return render_template('catalogue/index.html',
                            categories=Category.query.all(),
                            products=Product.query.all())
 
 
-@catalogue.route("catalogue/")
 def catalogue_view():
     return render_template('catalogue/catalogue.html',
                            products=Product.query.all())
 
 
-@catalogue.route("catalogue/<product_id>/")
 def product_detail(product_id):
     return render_template('catalogue/product_detail.html',
                            product=Product.query.get(product_id))
+
+
+catalogue.add_url_rule('/', 'index', index)
+catalogue.add_url_rule('catalogue/', 'catalogue', catalogue_view)
+catalogue.add_url_rule('catalogue/<product_id>/', 'product_detail',
+                       product_detail)
