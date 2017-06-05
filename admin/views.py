f252b724d30f6ea87e90f83bc9a17a7f3d2b6c32
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-05 15:01:57 +0300

    add payment model to admin

--- admin/views.py
+++ admin/views.py
@@ -4,6 +4,7 @@ from flask_admin.contrib.sqla import ModelView
 from catalogue.models import Product, Category
 from database import db
 from users.models import User
+from payment.models import Payment
 
 
 def configure_admin_views(app):
@@ -11,3 +12,4 @@ def configure_admin_views(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Product, db.session))
     admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Payment, db.session))
