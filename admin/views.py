d6a998a0c021c53ab14341ea99a74233bec09cd5
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-05 16:16:53 +0300

    add dicount to admin

--- admin/views.py
+++ admin/views.py
@@ -3,8 +3,9 @@ from flask_admin.contrib.sqla import ModelView
 
 from catalogue.models import Product, Category
 from database import db
-from users.models import User
+from order.models import Discount
 from payment.models import Payment
+from users.models import User
 
 
 def configure_admin_views(app):
@@ -13,3 +14,4 @@ def configure_admin_views(app):
     admin.add_view(ModelView(Product, db.session))
     admin.add_view(ModelView(Category, db.session))
     admin.add_view(ModelView(Payment, db.session))
+    admin.add_view(ModelView(Discount, db.session))
