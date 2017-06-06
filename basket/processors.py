fc9540b4b3e4619242b9c7f433b3272b6634aac7
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-06 10:24:43 +0300

    prevent crashing app by Anonymous user

--- basket/processors.py
+++ basket/processors.py
@@ -2,5 +2,8 @@ from flask_login import current_user
 
 
 def basket_processor():
-    count = current_user.get_basket.lines_count
+    try:
+        count = current_user.get_basket.lines_count
+    except AttributeError:
+        return {}
     return {'basket_lines_count': count}
