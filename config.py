b0e289bece482ed1b88b49dc704d0e8ecd964c1a
Dmitry-Kiselev <kdem27@gmail.com>
2017-06-06 09:08:48 +0300

    set SQLALCHEMY_TRACK_MODIFICATIONS to False

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = '2sXBwWL5CeyG6ZcFBCe92zrb2LGetNwQ'
     SQLALCHEMY_DATABASE_URI = "postgresql://store:store@localhost/store"
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
